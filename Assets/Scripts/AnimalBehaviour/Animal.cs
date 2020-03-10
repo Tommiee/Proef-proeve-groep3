e05dd8dcd860e0c1c4f46c0163c7e8b12fed08a4
Tommiee <tomdepom1999@gmail.com>
2020-03-10 15:56:48 +0100

    Added animal catching + manager

---
+++ Assets/Scripts/AnimalBehaviour/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Animal : MonoBehaviour
+{
+    public delegate void onCaughtDelegate();
+    public event onCaughtDelegate OnCaught;
+
+    private ScreenInput _screenInput;
+    private AnimalManager _animalManager;
+    private Collider _collider;
+
+    public string _type;
+
+    void Start()
+    {
+        _screenInput = GameObject.FindObjectOfType<ScreenInput>();
+        _screenInput.OnPressed += CheckAnimal;
+
+        _animalManager = GameObject.FindObjectOfType<AnimalManager>();
+
+        _collider = gameObject.GetComponent<Collider>();
+    }
+
+    private void CheckAnimal(Collider _col)
+    {
+        if(_collider == _col)
+        {
+            AnimalCaught();
+        }
+    }
+
+    private void AnimalCaught()
+    {
+        OnCaught?.Invoke();
+        if(_animalManager != null)
+        {
+            _animalManager.SendMessage("CaughtAnimal", _type, SendMessageOptions.DontRequireReceiver);
+        }
+
+        //trigger anim
+
+        Destroy(this);
+    }
+}
