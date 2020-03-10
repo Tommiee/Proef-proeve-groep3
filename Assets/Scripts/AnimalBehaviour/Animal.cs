d7771a43603c2819b13cd9194f8678c2d064446b
Tommiee <tomdepom1999@gmail.com>
2020-03-10 11:17:04 +0100

    changed formatting

--- Assets/Scripts/AnimalBehaviour/Animal.cs
+++ Assets/Scripts/AnimalBehaviour/Animal.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class Animal : MonoBehaviour
-{
+public class Animal : MonoBehaviour {
     public delegate void onCaughtDelegate();
     public event onCaughtDelegate OnCaught;
 
@@ -17,8 +16,7 @@ public class Animal : MonoBehaviour
 
     public string _type;
 
-    void Start()
-    {
+    void Start() {
         _screenInput = GameObject.FindObjectOfType<ScreenInput>();
         _screenInput.OnPressed += CheckAnimal;
 
@@ -27,20 +25,16 @@ public class Animal : MonoBehaviour
         _collider = gameObject.GetComponent<Collider>();
     }
 
-    private void CheckAnimal(Collider _col)
-    {
-        if (_collider == _col)
-        {
+    private void CheckAnimal(Collider _col) {
+        if (_collider == _col) {
             AnimalCaught();
         }
     }
 
-    private void AnimalCaught()
-    {
+    private void AnimalCaught() {
         OnCaught?.Invoke();
 
-        if (_animalManager != null)
-        {
+        if (_animalManager != null) {
             _animalManager.SendMessage("CaughtAnimal", _type, SendMessageOptions.RequireReceiver);
             Instantiate(_catchEffect, transform.position, transform.rotation);
             Destroy(gameObject);
