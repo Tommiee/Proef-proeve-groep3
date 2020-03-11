4ac12e35d524dbdad8f5e6765ab4ad306eb78f73
JaccoJ <jacco.jongenotter@gmail.com>
2020-03-11 09:42:41 +0100

    Added Random Sizing

---
+++ Assets/Scripts/RandomSize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSize : MonoBehaviour
+{
+    [SerializeField]
+    private float _minSize;
+    [SerializeField]
+    private float _maxSize;
+    private float _randomSize;
+    private Vector3 _changeSize;
+    void Start()
+    {
+        _randomSize = Random.Range(_minSize, _maxSize);
+        _changeSize = new Vector3(_randomSize, _randomSize, _randomSize);
+    }
+
+    
+    void Update()
+    {
+        transform.localScale = _changeSize;
+        print(_changeSize);
+    }
+}
